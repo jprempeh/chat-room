8022daf1b6425c06a054b92c93586190597f44dc
Joe Prempeh <prempehjoe@gmail.com>
2015-05-05 21:53:52 -0700

    spa utilities added

---
+++ js/spa.util.js
@@ -0,0 +1,37 @@
+/*
+ * spa.util.js
+ * General JavaScript utilities
+ *
+ *
+ */
+
+/*jslint          browser : true,  continue : true,
+ devel  : true,  indent  : 2,     maxerr   : 50,
+ newcap : true,  nomen   : true,  plusplus : true,
+ regexp : true,  sloppy  : true,  vars     : false,
+ white  : true
+ */
+/*global $, spa */
+
+spa.util = (function () {
+    var makeError, setConfigMap;
+
+    // Begin Public constructor /makeError/
+    // Purpose: a convenience wrapper to create an error object
+    // Arguments:
+    //   * name_text - the error name
+    //   * msg_text  - long error message
+    //   * data      - optional data attached to error object
+    // Returns  : newly constructed error object
+    // Throws   : none
+    //
+    makeError = function ( name_text, msg_text, data ) {
+        var error     = new Error();
+        error.name    = name_text;
+        error.message = msg_text;
+
+        if ( data ){ error.data = data; }
+
+        return error;
+    };
+// End Public constructor /makeError/
